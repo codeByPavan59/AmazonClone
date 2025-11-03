339170e93812eed69908c0fef30ec357de63b3ab
codebyPavan <pavankumar5911l@gmail.com>
2025-11-03 14:03:26 +0530

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -76,3 +76,14 @@ export function updateQuantity(productId, newQuantity) {
   saveToStorage();
 }
 
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open("GET", "https:supersimplebackend.dev/cart");
+  xhr.send();
+}
